b5958a871fc8ee840ec1e9225442c37e9994dd40
Unknown <c.ettorre@gmail.com>
2018-04-22 17:41:11 +0200

    creating ExoPLayer
    added SimpleExoPlayerView to activity_main.xml layout
    created SimpleExoPlayer instance
    instanciated SimpleExoPlayerView view using ExoPlayerFactory
    set Media Controller

--- app/src/main/java/com/example/cettorre/androidvideoplayer/MainActivity.java
+++ app/src/main/java/com/example/cettorre/androidvideoplayer/MainActivity.java
@@ -1,13 +1,79 @@
 package com.example.cettorre.androidvideoplayer;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.LoopingMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.hls.HlsMediaSource;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
+
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private SimpleExoPlayerView simpleExoPlayerView;
+    private SimpleExoPlayer player;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // TrackSelector
+        BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+        TrackSelection.Factory videoTrackSelectionFactory = new AdaptiveTrackSelection.Factory(bandwidthMeter);
+        TrackSelector trackSelector = new DefaultTrackSelector(videoTrackSelectionFactory);
+
+        //LoadControl
+        LoadControl loadControl = new DefaultLoadControl();
+
+        //Player
+        player = ExoPlayerFactory.newSimpleInstance(this, trackSelector, loadControl);
+        simpleExoPlayerView = new SimpleExoPlayerView(this);
+        simpleExoPlayerView = findViewById(R.id.player_view);
+
+        //Media controller
+        simpleExoPlayerView.setUseController(true);
+        simpleExoPlayerView.requestFocus();
+
+        // View
+        simpleExoPlayerView.setPlayer(player);
+
+        //Uri
+        Uri m3u8VideoUri = Uri.parse("http://www.streambox.fr/playlists/x36xhzz/x36xhzz.m3u8");
+
+        //It measures the bandwidth during playback.
+        DefaultBandwidthMeter bandwidthMeterA = new DefaultBandwidthMeter();
+
+        //Produces DataSource instances through which media data is loaded.
+        DefaultDataSourceFactory dataSourceFactory = new DefaultDataSourceFactory(this, Util.getUserAgent(this, "exoplayer2example"), bandwidthMeterA);
+
+        //MediaSource
+        MediaSource videoSource = new HlsMediaSource(m3u8VideoUri, dataSourceFactory, 1, null, null);
+
+        final LoopingMediaSource loopingSource = new LoopingMediaSource(videoSource);
+
+        // Prepare the player with the source.
+        player.prepare(loopingSource);
+
     }
+
+
 }
