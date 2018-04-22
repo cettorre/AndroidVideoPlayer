ab2e98abd07c426f306c61becbc7b457fce65f79
Unknown <c.ettorre@gmail.com>
2018-04-22 20:31:55 +0200

    working on listeners
    created Player.EventListener anonimous class and added as player listener
    testing listeners
    implemeted VideoRendererEventListener to get when first frame is rendered to meet requirement
            //Video shows up its first frame -> frame message

--- app/src/main/java/com/example/cettorre/androidvideoplayer/MainActivity.java
+++ app/src/main/java/com/example/cettorre/androidvideoplayer/MainActivity.java
@@ -2,29 +2,45 @@ package com.example.cettorre.androidvideoplayer;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Surface;
 import android.view.View;
 
 import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlaybackException;
 import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.Format;
 import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.PlaybackParameters;
+import com.google.android.exoplayer2.Player;
 import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.Timeline;
+import com.google.android.exoplayer2.decoder.DecoderCounters;
 import com.google.android.exoplayer2.source.LoopingMediaSource;
 import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.TrackGroupArray;
 import com.google.android.exoplayer2.source.hls.HlsMediaSource;
 import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
 import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
 import com.google.android.exoplayer2.trackselection.TrackSelector;
 import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
 import com.google.android.exoplayer2.upstream.BandwidthMeter;
 import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
 import com.google.android.exoplayer2.util.Util;
+import com.google.android.exoplayer2.video.VideoRendererEventListener;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements VideoRendererEventListener {
 
 
     private SimpleExoPlayerView simpleExoPlayerView;
@@ -73,7 +89,106 @@ public class MainActivity extends AppCompatActivity {
         // Prepare the player with the source.
         player.prepare(loopingSource);
 
+
+        player.addListener(new Player.EventListener() {
+
+
+            @Override
+            public void onLoadingChanged(boolean isLoading) {
+                //when video starts to load
+                //Video starts to load(first play only). -> start message
+
+                Log.i("videop", "Listener-onLoadingChanged...isLoading:" + isLoading);
+
+
+            }
+
+            @Override
+            public void onTimelineChanged(Timeline timeline, Object manifest) {
+
+            }
+
+            @Override
+            public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
+                //when finished
+
+                Log.i("videop", "Listener-onTracksChanged...");
+
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    Log.i("videop", "Listener-..." + player.getDuration() + LocalDateTime.now());
+                }
+
+
+            }
+
+            List<Long> timeList = new ArrayList<>();
+
+            @Override
+            public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+                Log.i("videop", "Listener-onPlayerStateChanged...");
+
+            }
+
+            @Override
+            public void onRepeatModeChanged(int repeatMode) {
+
+            }
+
+            @Override
+            public void onPlayerError(ExoPlaybackException error) {
+
+            }
+
+            @Override
+            public void onPositionDiscontinuity() {
+
+            }
+
+            @Override
+            public void onPlaybackParametersChanged(PlaybackParameters playbackParameters) {
+
+            }
+        });
     }
 
 
+    @Override
+    public void onVideoEnabled(DecoderCounters counters) {
+
+    }
+
+    @Override
+    public void onVideoDecoderInitialized(String decoderName, long initializedTimestampMs, long initializationDurationMs) {
+
+    }
+
+    @Override
+    public void onVideoInputFormatChanged(Format format) {
+
+    }
+
+    @Override
+    public void onDroppedFrames(int count, long elapsedMs) {
+
+    }
+
+    @Override
+    public void onVideoSizeChanged(int width, int height, int unappliedRotationDegrees, float pixelWidthHeightRatio) {
+
+    }
+
+    @Override
+    public void onRenderedFirstFrame(Surface surface) {
+        Log.i("videop", "Listener-onRenderedFirstFrame..." + surface);
+
+
+        //Video shows up its first frame -> frame message
+
+    }
+
+    @Override
+    public void onVideoDisabled(DecoderCounters counters) {
+
+    }
 }
